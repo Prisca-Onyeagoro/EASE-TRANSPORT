59cf8214cffba498521f2f3b60698236da3cfbc3
prisca-onyeagoro <priscaonyeagoro@gmail.com>
2025-12-15 11:29:06 +0100

    corrrectederror

--- app/(Home)/layout.jsx
+++ app/(Home)/layout.jsx
@@ -1,4 +1,4 @@
-import NavigationBar from "@/components/NavigationBar";
+import NavigationBar from "@/Components/NavigationBar";
 export default function HomeLayout({ children }) {
   return (
     <>
